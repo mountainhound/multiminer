bbeb700b5fb3ad2a5af787544a96276b2bbd7e84
Parker Roan <parker@fopark.com>
2018-05-14 13:22:54 -0500

    added kraken wallet address

--- profit_switcher.py
+++ profit_switcher.py
@@ -238,31 +238,34 @@ class multiminer():
 		return "No Algo Running"
 
 	def nvidia_temp_output(self):
+		try: 
+			sp = subprocess.Popen(['nvidia-smi', '-q','-x','-f','temp.xml'])
+			sp.wait()
 
-		sp = subprocess.Popen(['nvidia-smi', '-q','-x','-f','temp.xml'])
-		sp.wait()
+			with open('temp.xml') as fd:
+				doc = xmltodict.parse(fd.read())
 
-		with open('temp.xml') as fd:
-			doc = xmltodict.parse(fd.read())
+			doc = dict(doc)
+			gpu_list = doc.get('nvidia_smi_log').get('gpu')
+			temp_list = []
 
-		doc = dict(doc)
-		gpu_list = doc.get('nvidia_smi_log').get('gpu')
-		temp_list = []
-
-		if isinstance(gpu_list, dict): 
-			temp_dict = gpu_list.get('temperature')
-			temp_list.append(temp_dict.get('gpu_temp'))
-			
-		elif isinstance(gpu_list,list):
-			for gpu in gpu_list:
-				temp_dict = gpu.get('temperature')
+			if isinstance(gpu_list, dict): 
+				temp_dict = gpu_list.get('temperature')
 				temp_list.append(temp_dict.get('gpu_temp'))
+				
+			elif isinstance(gpu_list,list):
+				for gpu in gpu_list:
+					temp_dict = gpu.get('temperature')
+					temp_list.append(temp_dict.get('gpu_temp'))
 
-		sp = subprocess.Popen(['rm','temp.xml'])
-		sp.wait()
+			sp = subprocess.Popen(['rm','temp.xml'])
+			sp.wait()
 
-		return temp_list
-
+			return temp_list
+			
+		except Exception as err: 
+			print (err)
+			return None
 	def ccminer_api_output(self,command = b"summary"):
 		try: 
 			tn = Telnet("localhost","4068")
